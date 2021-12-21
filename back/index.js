21e641db7e639919a4c7b5f2ad78797233b125f6
necmigunduz <necmigunduz@gmail.com>
2021-12-21 18:12:03 +0300

    Add dotenv

--- back/index.js
+++ back/index.js
@@ -1,9 +1,10 @@
 // Index.js
 let express = require('express')
+require('dotenv').config()
 
 let app = express();
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8081;
 
 app.get('/', (req, res) => res.send('Hello World with Express'));
 app.listen(port, function () {
