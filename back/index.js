18bb9eae0b3a60bddfb73d3e65fe4afb1b0e7851
necmigunduz <necmigunduz@gmail.com>
2021-12-21 18:04:41 +0300

    Add Index to the back and nodemon

---
+++ back/index.js
@@ -0,0 +1,11 @@
+// Index.js
+let express = require('express')
+
+let app = express();
+
+var port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => res.send('Hello World with Express'));
+app.listen(port, function () {
+     console.log("Running RestHub on port " + port);
+});
