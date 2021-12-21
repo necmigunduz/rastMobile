98122f994d801e24fa408ede01bddbbb543b2b8d
necmigunduz <necmigunduz@gmail.com>
2021-12-21 12:58:22 +0300

    Add styling, icons, and upload

--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,14 @@
+import "./style.css";
+import json from "./data.json5";
+
+console.log(json.title);
+console.log(json.owner.name);
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement("div");
+
+  element.innerHTML = "Hello from WebPack";
+
+  return element;
+}
+
+document.body.appendChild(component());
