e2966f702e634c7cb2575a9a263b1d98c330f950
necmigunduz <necmigunduz@gmail.com>
2021-12-22 10:13:56 +0300

    Add api routes

--- back/index.js
+++ back/index.js
@@ -1,10 +1,12 @@
 // Index.js
 let express = require('express')
+let apiRoutes = require("./api-routes")
 require('dotenv').config()
 
 let app = express();
 
 var port = process.env.PORT || 8081;
+app.use('/api', apiRoutes)
 
 app.get('/', (req, res) => res.send('Hello World with Express'));
 app.listen(port, function () {
