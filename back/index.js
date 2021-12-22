9c812cba893eb5b42be11c8b771f3d12dab1e450
necmigunduz <necmigunduz@gmail.com>
2021-12-22 10:21:23 +0300

    Configure mongodb and body-parser

--- back/index.js
+++ back/index.js
@@ -1,14 +1,30 @@
 // Index.js
-let express = require('express')
-let apiRoutes = require("./api-routes")
-require('dotenv').config()
+let express = require("express");
+let apiRoutes = require("./api-routes");
+let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
+require("dotenv").config();
 
 let app = express();
 
-var port = process.env.PORT || 8081;
-app.use('/api', apiRoutes)
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.get('/', (req, res) => res.send('Hello World with Express'));
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost/resthub", { useNewUrlParser: true });
+var db = mongoose.connection;
+
+if (!db) console.log("Error connecting db");
+else console.log("Db connected successfully");
+
+var port = process.env.PORT || 8081;
+
+app.get("/", (req, res) => res.send("Hello World with Express"));
+app.use("/api", apiRoutes);
 app.listen(port, function () {
-     console.log("Running RestHub on port " + port);
+  console.log("Running RestHub on port " + port);
 });
