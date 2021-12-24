6deee771eda8f148404d5e31dc52f83718feebeb
necmigunduz <necmigunduz@gmail.com>
2021-12-24 12:21:44 +0300

    Add create method to backlog controller

--- back/Controllers/backLogController.js
+++ back/Controllers/backLogController.js
@@ -1,5 +1,5 @@
 // BackLog Controller
-BackLog = require('../Models/backLogModel')
+import BackLog from '../Models/backLogModel'
 
 // Index
 exports.index = function(req, res) {
@@ -21,5 +21,20 @@ exports.index = function(req, res) {
 
 // Create
 exports.new = function(req, res) {
-    
+    let backLog = new BackLog()
+    backLog.name = req.body.name ? req.body.name : backLog.name
+    backLog.description = req.body.description
+    backLog.logType = req.body.logType
+    backLog.color = req.body.color
+
+    // Save backlog
+    backLog.save(function(err){
+        if(err)
+            res.json(err)
+
+        res.json({
+            message: "New backlog is successfully created!",
+            data: backLog
+        })
+    })
 }
