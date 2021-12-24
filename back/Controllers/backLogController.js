10b7c529ca288d9741898f34568e3b3d6df54f7c
necmigunduz <necmigunduz@gmail.com>
2021-12-24 12:12:48 +0300

    Fix backlog controller index method

--- back/Controllers/backLogController.js
+++ back/Controllers/backLogController.js
@@ -2,38 +2,24 @@
 BackLog = require('../Models/backLogModel')
 
 // Index
-export function index(req, res) {
+exports.index = function(req, res) {
     BackLog.get(function(err, backLogs){
-        if(err) {
+        if(err){
             res.json({
                 status: "error",
-                message: err,                
+                message: err
             })
         }
+
         res.json({
             status: "success",
-            message: "Backlogs are retrieved successfully!",
+            message: "Backlogs retrieved successfully!",
             data: backLogs
         })
     })
 }
 
 // Create
-
-export function create(req, res) {
-    let backLog = new BackLog()
-    backLog.name = req.body.name ? req.body.name : backLog.name
-    backLog.description = req.body.description
-    backLog.logType = req.body.logType
-    backLog.color = req.body.color
-
-    // Save created backlog
-    backLog.save(function(err){
-        if(err)
-            res.json(err)
-        res.json({
-            message: "New backlog is created successfully!",
-            data: backLog
-        })
-    })
+exports.new = function(req, res) {
+    
 }
